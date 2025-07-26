b076ca17fa1a6e99dc98fa484c7be353c6d8f0d2
EndersJr01 <enders.ramli@gmail.com>
2025-07-26 20:33:15 +0700

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GameBoard from "@/components/game/GameBoard";
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <GameBoard />
+    </main>
+  );
+};
+
+export default Home;
