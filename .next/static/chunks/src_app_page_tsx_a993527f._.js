5aa97d12ed7800f3c41ebc858e515131a2a92f4e
EndersJr01 <enders.ramli@gmail.com>
2025-07-26 20:32:53 +0700

    Delete .next/static/chunks directory

--- .next/static/chunks/src_app_page_tsx_a993527f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_227bc5d6._.js",
-  "static/chunks/src_ab55ce4e._.js"
-],
-    source: "dynamic"
-});
